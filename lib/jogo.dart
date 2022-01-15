b078b2460205e264a5658cac9faf50dd088f4a6a
Luciano Paiva <lucianopaiva.e@gmail.com>
2022-01-15 17:22:00 -0300

    Utilizando setState para alterar variaveis e definir as validações

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Jogo extends StatefulWidget {
   @override
@@ -6,6 +7,57 @@ class Jogo extends StatefulWidget {
 }
 
 class _JogoState extends State<Jogo> {
+  var _imageApp = AssetImage("imagens/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo";
+
+  void opcaoSelecionada(String escolhaUsuario) {
+    //print("Você escolheu: " + escolhaUsuario);
+    var opcoes = ["Pedra", "Papel", "Tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+    print("Escolha do Bot " + escolhaApp);
+    print("Escolha do Usuário " + escolhaUsuario);
+
+    switch (escolhaApp) {
+      case "Pedra":
+        setState(() {
+          this._imageApp = AssetImage("imagens/pedra.png");
+        });
+        break;
+      case "Papel":
+        setState(() {
+          this._imageApp = AssetImage("imagens/papel.png");
+        });
+        break;
+      case "Tesoura":
+        setState(() {
+          this._imageApp = AssetImage("imagens/tesoura.png");
+        });
+        break;
+    }
+    //Validando o resultado
+    //Se o o usuario vencer
+    if ((escolhaUsuario == "Pedra" && escolhaApp == "Tesoura") ||
+        (escolhaUsuario == "Tesoura" && escolhaApp == "Papel") ||
+        (escolhaUsuario == "Papel" && escolhaApp == "Pedra")) {
+      setState(() {
+        this._mensagem = "Parabéns! Você ganhou";
+      });
+    } //Se o app vencer
+    else if ((escolhaApp == "Pedra" && escolhaUsuario == "Tesoura") ||
+        (escolhaApp == "Tesoura" && escolhaUsuario == "Papel") ||
+        (escolhaApp == "Papel" && escolhaUsuario == "Pedra")) {
+      setState(() {
+        this._mensagem = "É uma pena! Você perdeu";
+      });
+    } else {
+      setState(() {
+        this._mensagem = "Você empatou! Tente novamente";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,21 +89,13 @@ class _JogoState extends State<Jogo> {
             ),
           ),
 //  Escolha do app
-          GestureDetector(
-            onTap: () {
-              print("Um clique");
-            },
-            onDoubleTap: () {
-              print("Dois cliques");
-            },
-            child: Image.asset("imagens/padrao.png"),
-          ),
+          Image(image: this._imageApp),
 
 //  Resultado
           Padding(
             padding: EdgeInsets.only(top: 32, bottom: 16),
             child: Text(
-              "Escolha uma das opções abaixo",
+              this._mensagem,
               textAlign: TextAlign.center,
               style: TextStyle(
                   fontSize: 25,
@@ -63,6 +107,28 @@ class _JogoState extends State<Jogo> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              GestureDetector(
+                onTap: () => opcaoSelecionada("Pedra"),
+                child: Image.asset(
+                  "imagens/pedra.png",
+                  height: 90,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => opcaoSelecionada("Papel"),
+                child: Image.asset(
+                  "imagens/papel.png",
+                  height: 90,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => opcaoSelecionada("Tesoura"),
+                child: Image.asset(
+                  "imagens/tesoura.png",
+                  height: 90,
+                ),
+              ),
+              /*
               Image.asset(
                 "imagens/pedra.png",
                 height: 90,
@@ -74,7 +140,7 @@ class _JogoState extends State<Jogo> {
               Image.asset(
                 "imagens/tesoura.png",
                 height: 90,
-              ),
+              ),*/
             ],
           ),
         ],
